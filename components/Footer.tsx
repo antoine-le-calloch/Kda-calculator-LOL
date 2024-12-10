8e5f4419959d7fbd31c61309461b246d0c787481
antoine-le-calloch <antoine.lecalloch@gmail.com>
2024-12-10 02:38:39 +0100

    Move header code into a separate file and create footer.

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div className="bg-gradient-to-b from-blue-950 to-black flex flex-col items-center
+            text-white text-xs italic pt-3 pb-2 text-center">
+                <p>© 2024 - All rights reserved. Developed by Antoine Le Calloch.</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <header>
+            <div className="flex flex-col items-center pt-8 pb-4">
+                <h1 className="text-white italic">KDA CALCULATOR</h1>
+                <h2 className="text-yellow-500">League of Legends</h2>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
