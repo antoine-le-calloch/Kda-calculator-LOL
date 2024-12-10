f07c8a6221c61d06ebe4728fe4d8e3f451657b2c
antoine-le-calloch <antoine.lecalloch@gmail.com>
2024-12-10 02:56:36 +0100

    Add export in front of global interface and remove interface that is not global.

--- components/types/global.ts
+++ components/types/global.ts
@@ -1,20 +1,10 @@
-interface Kda {
+export interface Kda {
     kills: number;
     deaths: number;
     assists: number;
 }
 
-interface Profile {
-    name: string;
-    tagline: string;
-}
-
-interface Period {
-    startDate: string;
-    endDate: string;
-}
-
-interface MatchesData {
+export interface MatchesData {
     nbMatches: number;
     kdaList: Kda[];
     kdaTotal: Kda;
